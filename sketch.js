d175d5fd7f32e90d085b9692c8d205724f9cca73
raquel cardoso <72284830+rahqueu@users.noreply.github.com>
2022-07-01 15:09:34 +0100

    final sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
 
 // Database (CHANGE THESE!)
 const GROUP_NUMBER   = "44-AL";      // Add your group number here as an integer (e.g., 2, 3)
-const BAKE_OFF_DAY   = false;  // Set to 'true' before sharing during the bake-off day
+const BAKE_OFF_DAY   = true;  // Set to 'true' before sharing during the bake-off day
 
 // Target and grid properties (DO NOT CHANGE!)
 let PPI, PPCM;
@@ -29,12 +29,18 @@ let current_trial    = 0;      // the current trial number (indexes into trials 
 let attempt          = 0;      // users complete each test twice to account for practice (attemps 0 and 1)
 let fitts_IDs        = [];     // add the Fitts ID for each selection here (-1 when there is a miss)
 
-let aimbot = false;
+// Real cursor
 let aimbotX;
 let aimbotY;
 let aimbotW;
+let aimbot = false;
+
+// Etc.
 let bg;
 let sound;
+let end_sound;
+let x_clicks = new Array(54);
+let y_clicks = new Array(54);
 
 // Target class (position and width)
 class Target
@@ -50,10 +56,11 @@ class Target
 // Runs once at the start
 function setup()
 {
-    
+  
   bg = loadImage('https://media.discordapp.net/attachments/897573939977535529/961299972148563978/unknown.png');
   sound = loadSound('y2mate (mp3cut.net) (2).mp3');
-  
+  end_sound = loadSound('X2Download (mp3cut.net) (1).mp3');
+
   createCanvas(700, 500);    // window size in px before we go into fullScreen()
   frameRate(60);             // frame rate (DO NOT CHANGE!)
   
@@ -61,16 +68,35 @@ function setup()
   
   textFont("Arial", 18);     // font size for the majority of the text
   drawUserIDScreen();        // draws the user start-up screen (student ID and display size)
+  
 }
+
+
+function setLineDash(list) {
+    drawingContext.setLineDash(list);
+  }
 
 // Runs every frame and redraws the screen
 function draw()
 {
+  
   if (draw_targets)
   {     
-    // The user is interacting with the 6x3 target grid
     
+    // The user is interacting with the 6x3 target grid
+
     background(bg);
+    
+    strokeWeight(3);
+    fill(color(0, 240, 255));
+    text("Current Target:", width/1.71, height/3.3);
+    fill(color(0,240,255));
+    circle(width/1.44, height/3.35 , TARGET_SIZE*0.85);
+    strokeWeight(3);
+    fill(color(253, 202, 64));
+    text("Next Target:", width/1.67, height/2.4);
+    fill(color(253, 202, 64));
+    circle(width/1.44, height/2.45, TARGET_SIZE*0.85);
     
     // Print trial count at the top left-corner of the canvas
     fill(color(255,255,255));
@@ -78,21 +104,49 @@ function draw()
     strokeWeight(1);
     text("Trial " + (current_trial + 1) + " of " + trials.length, 50, 20);
     
+    if (!aimbot) {
+      let target = getTargetBounds(0);
+      aimbotX = target.x;
+      aimbotY = target.y;
+      aimbotW = target.w * 0.25;
+      aimbot = true;
+    }
+    
     // Draw all 18 targets
 	for (var i = 0; i < 18; i++) {
       drawTarget(i);
       drawSquare(i);
     }
-    
-    // Draw the user input area
-    drawInputArea()
 
-    // Draw the virtual cursor
-    let x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width)
-    let y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height)
+    // Draw the user input area
+    drawInputArea();
     
-    fill(color(255,20,147));
+    // Draw the virtual cursor
+    let x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width);
+    let y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height);
+    
+    setAimbot(x, y);
+    
+    // Draw aimbot cursor
+    fill(color(255,143,0));
+    noStroke();
+    circle(aimbotX, aimbotY, aimbotW);
+    
+    // Draw virtual cursor
+    fill(color(	255, 0, 240));
     circle(x, y, 0.5 * PPCM);
+  }
+}
+
+function setAimbot(x, y) {
+  for (var i = 0; i < 18; i++) {
+    let target = getTargetBounds(i);
+    if (abs(target.x - x) < target.w && abs(target.y - y) < target.w) {
+      aimbotX = target.x;
+      aimbotY = target.y;
+      aimbotW = target.w * 0.25;
+      aimbot = true;
+    }
   }
 }
 
@@ -106,6 +160,7 @@ function printAndSavePerformance()
   let penalty           = constrain((((parseFloat(95) - (parseFloat(hits * 100) / parseFloat(hits + misses))) * 0.2)), 0, 100);
   let target_w_penalty	= nf(((test_time) / parseFloat(hits + misses) + penalty), 0, 3);
   let timestamp         = day() + "/" + month() + "/" + year() + "  " + hour() + ":" + minute() + ":" + second();
+  let write_y = 300;
   
   strokeWeight(1);
   background(color('black'));
@@ -120,10 +175,22 @@ function printAndSavePerformance()
   text("Total time taken: " + test_time + "s", width/2, 160);
   text("Average time per target: " + time_per_target + "s", width/2, 180);
   text("Average time for each target (+ penalty): " + target_w_penalty + "s", width/2, 220);
+  end_sound.play();
+  aimbot = false;
   
-  // Print Fitts IDS (one per target, -1 if failed selection, optional)
-  // 
-
+  for (var i = 0; i < fitts_IDs.length/2; i++) {
+    if (i === 0) text("Target " + (i + 1) + ": ------", width/3, write_y);
+    else if (fitts_IDs[i] === -1) text("Target " + (i + 1)  + ": MISSED", width/3, write_y);
+    else text("Target " + (i + 1)  + ": " + fitts_IDs[i], width/3, write_y);
+    write_y = write_y + 20;
+  }
+  write_y = 300;
+  for (; i < fitts_IDs.length; i++) {
+    if (fitts_IDs[i] === -1) text("Target " + (i + 1)  + ": MISSED", 2*width/3, write_y);
+    else text("Target " + (i + 1)  + ": " + fitts_IDs[i], 2*width/3, write_y);
+    write_y = write_y + 20;
+  }
+  
   // Saves results (DO NOT CHANGE!)
   let attempt_data = 
   {
@@ -171,15 +238,19 @@ function mousePressed()
         
     if (insideInputArea(mouseX, mouseY))
     {
-      let virtual_x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width)
-      let virtual_y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height)
-
+      x_clicks[current_trial] = aimbotX;
+      y_clicks[current_trial] = aimbotY;
       if (dist(target.x, target.y, aimbotX, aimbotY) < target.w/2) {
         if (sound.isPlaying()) sound.stop();
         sound.play();
+        if (current_trial === 0) fitts_IDs.push(-1);
+        else fitts_IDs.push(getFittsId());
         hits++;
       }
-      else misses++;
+      else {
+        misses++;
+        fitts_IDs.push(-1);
+      }
       current_trial++; // Move on to the next trial/target
     }
 
@@ -189,7 +260,7 @@ function mousePressed()
       testEndTime = millis();
       draw_targets = false;          // Stop showing targets and the user performance results
       printAndSavePerformance();     // Print the user's results on-screen and send these to the DB
-      attempt++;                      
+      attempt++;
       
       // If there's an attempt to go create a button to start this
       if (attempt < 2)
@@ -198,8 +269,21 @@ function mousePressed()
         continue_button.mouseReleased(continueTest);
         continue_button.position(width/2 - continue_button.size().width/2, height/2 - continue_button.size().height/2);
       }
-    } 
+    }
+    else if (current_trial === 1) testStartTime = millis();
   }
+}
+
+// Returns the fitts ID of a certain target
+function getFittsId() {
+  
+  let target = getTargetBounds(trials[current_trial]);
+  let current = createVector(x_clicks[current_trial], y_clicks[current_trial]);
+  let previous = createVector(x_clicks[current_trial - 1], y_clicks[current_trial - 1]);
+  let distance = dist(current.x, current.y, previous.x, previous.y);
+  
+  return Math.log2(distance / target.w + 1).toFixed(3);
+  
 }
 
 // Draw target on-screen
@@ -211,28 +295,20 @@ function drawTarget(i)
   let virtual_x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width);
   let virtual_y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height);
   
-  if (abs(target.x - virtual_x) < target.w && abs(target.y - virtual_y) < target.w) {
-    aimbotX = target.x;
-    aimbotY = target.y;
-    aimbotW = target.w * 0.25;
-    aimbot = true;
-  }
-  else aimbot = false;
-  
   // Check whether this target is the target the user should be trying to select
   if (trials[current_trial] === i)
   { 
     // Highlights the target the user should be trying to select
     // with a purple border
-    stroke(color(191,64,191));
+    stroke(color(255, 0, 240));
     strokeWeight(5);
     // If the virtual mouse is within the target's boundaries, the target is green and cyan otherwise
-    if (dist(target.x, target.y, aimbotX, aimbotY) < target.w/2 && aimbot) fill(color(0, 255, 0));
-    else fill(color(0,255,255));
+    if (dist(target.x, target.y, aimbotX, aimbotY) < target.w/2) fill(color(0,255,15));
+    else fill(color(0,240,255));
     circle(target.x, target.y, target.w);
     // If the next target is also the current target, write '2x' on it
     if (trials[current_trial] === trials[current_trial+1]) {
-      fill(color(191,64,191));
+      fill(color(255, 255, 255));
       strokeWeight(1);
       textSize(20);
       text('2x', target.x - 12, target.y + 5);
@@ -241,39 +317,35 @@ function drawTarget(i)
   // Next target
   else if (trials[current_trial+1] === i) {
     // Purple border
-    stroke(color(191,64,191));
+    stroke(color(255, 0, 240));
     strokeWeight(5);
     // Yellow color
-    fill(color(255,255,0));
+    fill(color(253, 202, 64));
+    setLineDash([10, 10]);
     circle(next_target.x, next_target.y, next_target.w);
   }
   // Does not draw a border if this is not the target the user
   // should be trying to select
   else {
     noStroke();
-    fill(color(255, 255, 0));
+    fill(color(255, 233, 173));
     circle(target.x, target.y, target.w);
-  }
-  
-  // Draw aimbot cursor
-  if (aimbot) {
-    fill(color('black'));
-    noStroke();
-    circle(aimbotX, aimbotY, aimbotW);
   }
   
   noFill();
   strokeWeight(3);
-  stroke(color('gray'));
+  stroke(color(220,220,220));
   // Draws a grey grid around the targets
+  setLineDash([0, 0]);
   rect(target.x-target.w, target.y-target.w, target.w*2, target.w*2)
   
   if (i === 17) {
     target = getTargetBounds(trials[current_trial]);
     next_target = getTargetBounds(trials[current_trial+1]);
     // Draw a line from the current target to the next target
-    stroke(color(191,64,191));
+    stroke(color(255, 0, 240));
     strokeWeight(5);
+    setLineDash([10, 10]);
     line(target.x, target.y, next_target.x, next_target.y);
   }
 }
@@ -281,6 +353,7 @@ function drawTarget(i)
 function drawSquare(i) {
   
   let target = getTargetBounds(i);
+  let size = target.w/1.63;
   // Top left edge of the square surrounding the current target
   let v1 = createVector(target.x - target.w, target.y - target.w);
   let mini_squareX = map(v1.x, 0, width, inputArea.x, inputArea.x + inputArea.w, true);
@@ -298,13 +371,14 @@ function drawSquare(i) {
   
   if (trials[current_trial] === i) {
     
-    if (abs(mini_v1.x - mouseX) < target.w/1.67 && abs(mini_v1.y - mouseY) < target.w/1.67) fill(color(0, 255, 0));
-    else fill(color('cyan'));
-    stroke(color('cyan'));
+    if (abs(mini_v1.x + size/2 - mouseX) < size/2 && abs(mini_v1.y + size/2 - mouseY) < size/2) fill(color(0, 255, 15));
+    else fill(color(0,240,255));
+    stroke(color(255, 0, 240));
     strokeWeight(3);
-    rect(mini_v1.x, mini_v1.y, target.w/1.67, target.w/1.67);
+    setLineDash([0, 0]);
+    rect(mini_v1.x, mini_v1.y, size, size);
     if (trials[current_trial] === trials[current_trial+1]) {
-      fill(color('191,64,191'));
+      fill(color(255,15,0));
       strokeWeight(1);
       textSize(20);
       text('2x', mini_v1.x + 9, mini_v1.y + 27);
@@ -312,16 +386,16 @@ function drawSquare(i) {
   }
   else if (trials[current_trial+1] === i) {
     // Yellow color
-    stroke(color('gray'));
-    strokeWeight(3);
-    fill(color('purple'));
-    rect(next_mini_v1.x, next_mini_v1.y, target.w/1.67, target.w/1.67);
+    noStroke();
+    fill(color(253, 202, 64));
+    setLineDash([0, 0]);
+    rect(next_mini_v1.x, next_mini_v1.y, size, size);
   }
   else {
-    stroke('gray');
-    strokeWeight(3);
-    fill(color('yellow'));
-    rect(mini_v1.x, mini_v1.y, target.w/1.67, target.w/1.67);
+    noStroke();
+    fill(color(220,220,220));
+    setLineDash([0, 0]);
+    rect(mini_v1.x, mini_v1.y, size, size);
   }
 }
 
